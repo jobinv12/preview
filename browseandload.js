600c7530a8826fb9b939493e17cf298e408e2e7f
jobin thavu varghese <jobinthavuvarghese@jobins-MacBook-Air.local>
2020-12-09 12:45:06 +0530

    Updates

--- browseandload.js
+++ browseandload.js
@@ -3,18 +3,24 @@ var fs = require("fs");
 var filepath;
 var filename;
 
+// function browseGraph2D() {
+//   filepath = document.getElementById("filepath").value;
+//   // var splits = filepath.split("fakepath\\");
+//   alert(filepath);
+
+//   var layout = {
+//     title: "2d graph",
+//   };
+
+//   dfd.read_csv(filepath).then((df) => {
+//     df.plot("div1").line({ layout: layout });
+//   });
+// }
+
 function browseGraph2D() {
-  filepath = document.getElementById("filepath").value;
-  // var splits = filepath.split("fakepath\\");
-  alert(filepath);
-
-  var layout = {
-    title: "2d graph",
-  };
-
-  dfd.read_csv(filepath).then((df) => {
-    df.plot("div1").line({ layout: layout });
-  });
+  alert("This feature is under development.");
 }
 
-function browseGraph3D() {}
+function browseGraph3D() {
+  alert("This feature is under development.");
+}
