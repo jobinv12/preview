97606f43674391bffa3b6897130020aedb4d590b
jobin thavu varghese <jobinthavuvarghese@jobins-MacBook-Air.local>
2020-12-09 12:41:14 +0530

    preview

---
+++ dist/browseandload.js
@@ -0,0 +1,20 @@
+var dfd = require("danfojs-node");
+var fs = require("fs");
+var filepath;
+var filename;
+
+function browseGraph2D() {
+  filepath = document.getElementById("filepath").value;
+  // var splits = filepath.split("fakepath\\");
+  alert(filepath);
+
+  var layout = {
+    title: "2d graph",
+  };
+
+  dfd.read_csv(filepath).then((df) => {
+    df.plot("div1").line({ layout: layout });
+  });
+}
+
+function browseGraph3D() {}
