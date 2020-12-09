b8324732d8306033cfe695a4d3991e43d9e03520
jobin thavu varghese <jobinthavuvarghese@jobins-MacBook-Air.local>
2020-12-09 12:48:52 +0530

    Updates

--- browseandload.js
+++ browseandload.js
@@ -1,21 +1,4 @@
 var dfd = require("danfojs-node");
-var fs = require("fs");
-var filepath;
-var filename;
-
-// function browseGraph2D() {
-//   filepath = document.getElementById("filepath").value;
-//   // var splits = filepath.split("fakepath\\");
-//   alert(filepath);
-
-//   var layout = {
-//     title: "2d graph",
-//   };
-
-//   dfd.read_csv(filepath).then((df) => {
-//     df.plot("div1").line({ layout: layout });
-//   });
-// }
 
 function browseGraph2D() {
   alert("This feature is under development.");
